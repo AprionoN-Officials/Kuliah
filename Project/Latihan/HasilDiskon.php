b07c04a300e3ea063eddeeaa3717aed022ec1072
AprionoN <aprionon@gmail.com>
2025-12-06 20:08:43 +0700

    Penambahan HasilDiskon.php dan Penyempurnaan LatihanDiskon.php

---
+++ Project/Latihan/HasilDiskon.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hasil Transaksi</title>
+</head>
+<body>
+
+<?php
+    $nama   = $_POST['nama'];
+    $telp   = $_POST['telp'];
+    $alamat = $_POST['alamat'];
+    $barang = $_POST['barang'];
+    $satuan = $_POST['satuan'];
+    
+    $harga  = intval($_POST['harga']); 
+    $jumlah = intval($_POST['jumlah']); 
+
+    $total = $harga * $jumlah;
+
+    function hitung_diskon($total_harga)
+    {
+        if ($total_harga > 500000) {
+            return $total_harga * 0.05; 
+        } elseif ($total_harga > 250000) {
+            return $total_harga * 0.025;
+        } else {
+            return 0;
+        }
+    }
+    $disc = hitung_diskon($total);
+
+    $totBayar = $total - $disc;
+?>
+
+    <p>
+        <span class="label">Nama</span> : <?php echo $nama; ?><br><br>
+        
+        <span class="label">Telepon</span> : <?php echo $telp; ?><br><br>
+
+        <span class="label">Alamat</span> : <?php echo nl2br($alamat); ?><br><br>
+
+        <span class="label">Barang</span> : <?php echo $barang; ?><br><br>
+
+        <span class="label">Harga</span> : <?php echo number_format($harga, 0, ',', '.'); ?><br><br>
+
+        <span class="label">Jumlah</span> : <?php echo $jumlah; ?><br><br>
+
+        <span class="label">Satuan</span> : <?php echo ucfirst($satuan);?><br><br>
+
+        <span class="label">Total</span> : <?php echo number_format($total, 2, ',', '.'); ?><br><br>
+
+        <span class="label">Diskon</span> : <?php echo number_format($disc, 2, ',', '.'); ?><br><br>
+
+        <span class="label">Total Bayar</span> : <?php echo number_format($totBayar, 2, ',', '.'); ?>
+    </p>
+
+    <br>
+    <a href="LatihanDiskon.php">back</a> 
+
+</body>
+</html>
--- Project/Latihan/LatihanDiskon.php
+++ Project/Latihan/LatihanDiskon.php
@@ -1,34 +1,56 @@
 <!DOCTYPE html>
 <html>
+    <?php
+$satuan = ["buah","pcs","butir","kodi","set"];
+?>
 <head>
     <title>Form Transaksi</title>
 </head>
 <body>
 <h2>TOKO LAPTOP</h2>
-    <form action="hasilloop.php" method="post">
+    <form action="HasilDiskon.php" method="post">
         <table border="0">
             <tr>
                 <td>Nama </td>
                 <td>:</td>
-                <td><input type="text" name="nama"></td>
+                <td><input type="text" name="nama" size="20"></td>
             </tr>
             <tr>
-                <td>Jumlah</td>
+                <td>Telp</td>
                 <td>:</td>
-                <td><input type="text" name="jumlah[]"></td>
+                <td><input type="text" name="telp" size="20"></td>
+            </tr>
+            <tr>
+                <td>Alamat</td>
+                <td>:</td>
+                <td><textarea name="alamat" cols="22"></textarea></td>
+            </tr>
+            <tr>
+                <td>Barang</td>
+                <td>:</td>
+                <td><input type="text" name="barang" size="20"></td>
             </tr>
             <tr>
                 <td>Harga</td>
                 <td>:</td>
-                <td><input type="text" name="harga[]"></td>
+                <td><input type="text" name="harga" size="20"></td>
+            </tr>
+            <tr>
+                <td>Jumlah</td>
+                <td>:</td>
+                <td><input type="text" name="jumlah" size="10"></td>
+            </tr>
+            <tr>
+                <td>Satuan</td>
+                <td>:</td>
+                <td><select name="satuan"><?php foreach ($satuan as $s) { echo "<option value='$s'>$s</option>"; } ?></select>
             </tr>
             <tr>
                 <td></td>
                 <td></td>
                 <td>
                     <input type="submit" value="Proses"/>
-                    <input type="reset" value="Reset">
-                    <input type="button" value="Refresh" onclick="window.location.reload()">                
+                    <input type="reset" value="Cancel">
             </td>
             </tr>
         </table>
